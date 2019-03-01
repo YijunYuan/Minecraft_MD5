1fdda1eb2ecd220c17dd64f11cef5db08c982d66
袁轶君 <941201yuan@gmail.com>
2019-03-01 14:05:35 +0800

    Remove NSFW word.

--- Minecraft_md5/reset.h
+++ Minecraft_md5/reset.h
@@ -53,7 +53,7 @@ void inline reset() {
 	ofs << "kill @e[tag=md5]" << endl;
 	for (int i = 0; i < 32; i++) {
 		ofs <<
-			R"(summon armor_stand ~ 0 ~ {NoGravity:1b,Invisible:1,Invulnerable:1,CustomName:"\"fuck\"", Tags:["md5", "char)"
+			R"(summon armor_stand ~ 0 ~ {NoGravity:1b,Invisible:1,Invulnerable:1,CustomName:"\"*\"", Tags:["md5", "char)"
 			<< i << R"("]})" << endl;
 	}
 
